eac7ab2d6ab92beaee332a152976182e586bd919
Rcerruti <91542393+Rcerruti@users.noreply.github.com>
2021-12-11 22:00:39 +0100

    Update lib.h

--- lib.h
+++ lib.h
@@ -1,4 +1,8 @@
+//
+// Created by utente on 11/12/2021.
+//
+
 #ifndef ESERCITAZIONE_CERCA_PAROLA_ARRAY_LIB_H
 #define ESERCITAZIONE_CERCA_PAROLA_ARRAY_LIB_H
-bool funzione_nome(char [10][20], char [20], int & );
-#endif 
+bool funzione_nome(char a[10][20], char b[20], int & p);
+#endif //ESERCITAZIONE_CERCA_PAROLA_ARRAY_LIB_H
