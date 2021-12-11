d52bddffa58d51f7990cc89078fc8aee5795a4ad
Rcerruti <91542393+Rcerruti@users.noreply.github.com>
2021-12-11 20:50:46 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,17 @@
-
+#include "lib.h"
+int funzione_nome(char a[10][20], char b[20], int & p) {
+    int t=0;
+    for(int i=0; i<10; i++) {
+        t = 0;
+        for (int j = 0; j < 20; j++) {
+            if (a[i][j] == b[j]) {
+                t++;
+                if (t == 20) {
+                    p = i;
+                    return true;
+                }
+            }
+        }
+    }
+    return false;
+}
