89f81221360f1781aa9caddd034bacb5f12c4374
Rcerruti <91542393+Rcerruti@users.noreply.github.com>
2021-12-11 21:19:30 +0100

    Update lib.h

--- lib.h
+++ lib.h
@@ -1,4 +1,4 @@
 #ifndef ESERCITAZIONE_CERCA_PAROLA_ARRAY_LIB_H
 #define ESERCITAZIONE_CERCA_PAROLA_ARRAY_LIB_H
-bool funzione_nome(char [][20], char [], int & );
+bool funzione_nome(char a[][20], char b[], int & p);
 #endif 
