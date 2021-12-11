a29eabd1b4e765e898363fc1e8b8c05a0f667a6a
Rcerruti <91542393+Rcerruti@users.noreply.github.com>
2021-12-11 20:51:27 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,38 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
+
+void inizializzazione(char a[10][20]){
+    for(int i=0; i<10;i++){
+        for(int j=0; j<20;j++){
+            a[i][j]=0;
+        }
+    }
+}
+void inizializzazione2(char b[]){
+    for(int i=0; i<20;i++){
+        b[i]=0;
+    }
+}
+
+void riempimento(char nomi[10][20]){
+    for(int i=0; i<10;i++){
+        cin>>nomi[i];
+    }
+}
 
 int main(){
-  return 0;
+    char nomi[10][20];
+    char nome[20];
+    inizializzazione(nomi);
+    inizializzazione2(nome);
+    int posizione=0;
+    cin>>nome;
+    riempimento(nomi);
+    if(funzione_nome(nomi, nome, posizione)){
+        cout<<posizione;
+    }else{
+        cout<< "non presente";
+    }
+   return 0;
 }
